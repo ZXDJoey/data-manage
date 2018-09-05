8e6daf116a93fa3935eebf6219b390770b2e8c27
zxdjoey <774793337@qq.com>
2018-09-05 22:26:07 +0800

    修改main文件

--- src/main.js
+++ src/main.js
@@ -1,30 +1,20 @@
 import Vue from 'vue'
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import '../static/css/iconfont/1.0.0/index.css' // icofont
 
-// Echarts
-import echarts from 'echarts'
+import echarts from 'echarts' // Echarts
 Vue.prototype.$echarts = echarts
 import 'echarts/theme/macaron.js'
 
-// css
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 import '@/styles/index.scss' // global css
-
 import './mock' // simulation data
-
-// font-awesome
 import '@/assets/library/font-awesome-4.7.0/css/font-awesome.min.css'
-
 import App from './App'
-
 import router from './router'
-
 import store from './store'
-
 import i18n from './lang'
 
 import { loadStyle } from './utils/util'
@@ -33,11 +23,9 @@ import {
   iconfontVersion } from '@/config/env'
 
 import '@/icons'
-
 import '@/permission' // permission control
 
 import * as filters from './filters' // global filters
-// register global utility filters.
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
 })
@@ -45,7 +33,7 @@ Object.keys(filters).forEach(key => {
 Vue.use(ElementUI, { locale })
 iconfontVersion.forEach(ele => { loadStyle(iconfontUrl.replace('$key', ele)) })
 Vue.use(ElementUI, {
-  size: 'medium' // set element-ui default size
+  size: 'medium'
   // i18n: (key, value) => i18n.t(key, value)
 })
 
@@ -56,7 +44,5 @@ new Vue({
   router,
   store,
   i18n,
-  // template: '<App/>',
-  // components: { App }
   render: h => h(App)
 })
